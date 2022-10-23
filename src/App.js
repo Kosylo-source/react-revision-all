1b1a229aa92fa7bdbfa2d563bfa44f25a2561966
kpsylo-react <volodiakosylo@gmail.com>
2022-10-23 23:20:20 +0300

    hw2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Character from "./components/Character";
 
 const App = () => {
     return (
         <div>
-        <Character
-            id = {'2'}
-            />
+            
         </div>
     );
 };
