3f0636b60f47ff869941fb63fd04f50428d650f8
kpsylo-react <volodiakosylo@gmail.com>
2022-10-23 23:30:05 +0300

    hw2

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,17 +2,17 @@ import {useEffect, useState} from "react";
 
 
 const Posts = () => {
- const [post, setPost] = useState([])
-     useEffect( fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(value => value.json())
-        .then(value => {
-setPost(value)
-        }),[])
+ const [posts, setPost] = useState([])
+     useEffect(()=>{ fetch('https://jsonplaceholder.typicode.com/posts')
+             .then(value => value.json())
+             .then(value => {
+                 setPost(value)
+             })},[])
 
 
     return (
         <div>
-
+            post.map(post => {posts.id})
         </div>
     );
 };
